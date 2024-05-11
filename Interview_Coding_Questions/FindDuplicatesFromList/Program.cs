897bc9fa2b40800725044b4b740a61af508cd388
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-05-11 09:12:22 +0530

    Add files via upload

--- Interview_Coding_Questions/FindDuplicatesFromList/Program.cs
+++ Interview_Coding_Questions/FindDuplicatesFromList/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 
 public class Program
 {
+    //With Linq Query
     public static void DisplayDuplicatesOccurrence(int[] ints)
     {
         var occurrences = ints.GroupBy(x => x)
@@ -20,3 +21,47 @@ public class Program
         DisplayDuplicatesOccurrence(numbers);
     }
 }
+
+
+//With Dictionary
+/*
+ * using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static Dictionary<int, int> CountDuplicatesOccurrence(int[] ints)
+    {
+        Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+
+        foreach (int i in ints)
+        {
+            if (keyValuePairs.ContainsKey(i))
+            {
+                int value = keyValuePairs[i];
+                keyValuePairs[i] = ++value;
+            }
+            else
+            {
+                keyValuePairs.Add(i, 1);
+            }
+        }
+        
+        return keyValuePairs;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 2, 3, 4, 5, 6, 4, 2, 6, 8, 5, 3, 2, 3, 4, 5, 6, 2, 6, 8 };
+        Dictionary<int, int> occurrences = CountDuplicatesOccurrence(numbers);
+
+        foreach (var pair in occurrences)
+        {
+            Console.WriteLine($"Number: {pair.Key}, Occurrences: {pair.Value}");
+        }
+    }
+}
+
+ * 
+ */
