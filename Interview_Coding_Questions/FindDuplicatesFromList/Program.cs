6fe8c4c8bfe46bbec1e3161cabe5280d1b943263
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-05-11 09:08:23 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/FindDuplicatesFromList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static Dictionary<int, int> CountDuplicatesOccurrence(int[] ints)
+    {
+        Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+
+        foreach (int i in ints)
+        {
+            if (keyValuePairs.ContainsKey(i))
+            {
+                int value = keyValuePairs[i];
+                keyValuePairs[i] = ++value;
+            }
+            else
+            {
+                keyValuePairs.Add(i, 1);
+            }
+        }
+
+        return keyValuePairs;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 2, 3, 4, 5, 6, 4, 2, 6, 8, 5, 3, 2, 3, 4, 5, 6, 2, 6, 8 };
+        Dictionary<int, int> occurrences = CountDuplicatesOccurrence(numbers);
+
+        foreach (var pair in occurrences)
+        {
+            Console.WriteLine($"Number: {pair.Key}, Occurrences: {pair.Value}");
+        }
+    }
+}
