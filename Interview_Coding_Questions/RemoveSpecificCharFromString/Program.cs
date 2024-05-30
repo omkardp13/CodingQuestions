33185b2e6af025e8af7085a31491275e41ab8eb3
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-05-30 21:36:59 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/RemoveSpecificCharFromString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = "foo bar foo $ bar $ foo bar $ ";
+
+        // Find the positions of all dollar signs
+        int firstDollarIndex = input.IndexOf('$');
+        int secondDollarIndex = input.IndexOf('$', firstDollarIndex + 1);
+        int thirdDollarIndex = input.IndexOf('$', secondDollarIndex + 1);
+
+        // Remove the first and third dollar signs
+        if (firstDollarIndex != -1)
+        {
+            input = input.Remove(firstDollarIndex, 1);
+            // Adjust the second and third indexes after removing the first dollar sign
+            if (secondDollarIndex > firstDollarIndex) secondDollarIndex--;
+            if (thirdDollarIndex > firstDollarIndex) thirdDollarIndex--;
+        }
+
+        if (thirdDollarIndex != -1)
+        {
+            input = input.Remove(thirdDollarIndex, 1);
+        }
+
+        Console.WriteLine(input);
+        Console.ReadLine();
+    }
+}
