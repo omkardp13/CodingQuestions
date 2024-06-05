0d7a7ad6d1c591875d42bb763c1b76e784caa1e7
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 20:28:52 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/PrimeNumbers/FirstFivePrimeNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+
+    public static bool IsPrime(int num)
+    {
+        for (int i = 2; i < num; i++)
+        {
+            if(num %i==0)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+    
+
+    public static void Main(string[] args)
+    {
+        int counter = 0;
+        int num = 2;
+        
+        while (counter!=5) 
+        {
+            if(IsPrime(num))
+            {
+                Console.WriteLine(num);
+                counter++;
+            }
+            num++;  
+            
+        }
+        Console.ReadKey();
+    }
+}
