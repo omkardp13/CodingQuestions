a12fd785f5d1adf2187b9adadf16c7c6abdbc12c
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-06 08:14:43 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/ReverseString/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string to reverse:");
+        string input = Console.ReadLine();
+
+        string reversed = ReverseString(input);
+
+        Console.WriteLine("Reversed string: " + reversed);
+        Console.ReadLine();
+    }
+
+    static string ReverseString(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
