0e126f7252a8dc0902af9a4e22b327088db4104d
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-06 08:07:12 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/PrimeNumbers/FindPrimeNumbersUptoNth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+
+    public static void DisplayPrimeNumbers(int n)
+    {
+        bool isPrime = true; ;
+         for (int i = 2; i < n; i++) 
+         {
+            for (int j = 2;j < i;j++)
+            {
+                if(i%j==0)
+                {
+                    isPrime = false;
+                    break;
+                }
+
+            }
+            if(isPrime)
+             Console.WriteLine(i);
+
+            isPrime = true;
+         }
+    }
+    
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number:");
+        int num=Convert.ToInt32(Console.ReadLine());
+        DisplayPrimeNumbers(num);
+        Console.Read();
+    }
+}
