cbe8fe18f86c2ac2436fe8cf77189bbb7b0f9447
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-24 20:23:53 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/UsingRecursive/ReverseString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        
+    class program
+    {
+        
+        public static string ReverseString(string str)
+        {
+            if(str.Length<=1)
+            {
+                return str; 
+            }
+
+            return str[str.Length-1] + ReverseString(str.Substring(0,str.Length-1));
+        }
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter String:");
+            string str=Console.ReadLine();
+            Console.WriteLine(ReverseString(str));
+            Console.ReadLine();
+        }
+    }
+
+}
