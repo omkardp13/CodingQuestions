2a71f656f129eba24e36db015204a4cac9d7ec44
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-24 20:38:16 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/UsingRecursive/StringPalindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        
+    class program
+    {
+        
+        public static bool IsPalindromeString(string str)
+        {
+            if (str.Length <= 1)
+                return true;
+
+            if (str[0] != str[str.Length - 1])
+                return false;
+
+           return IsPalindromeString(str.Substring(1,str.Length-2));
+
+
+        }
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter String:");
+            string str=Console.ReadLine();
+            if(IsPalindromeString(str))
+                Console.WriteLine("String is palindrome");
+            else
+                Console.WriteLine("String is not palindrome");
+            Console.ReadLine();
+        }
+    }
+
+}
