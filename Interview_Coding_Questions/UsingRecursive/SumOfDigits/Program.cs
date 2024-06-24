7c3afe1d35e728be188429a759749d0735dc397e
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-24 19:50:43 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/UsingRecursive/SumOfDigits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        
+    class program
+    {
+        public static int Sum(int n)
+        {
+            if(n == 0) 
+              return 0;
+
+            return n%10+Sum(n/10);
+        }
+
+        public static void Main(string[] args) 
+        {
+            Console.Write("Enter the number of terms: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Sum(num));
+            
+            Console.ReadLine();
+        }
+    }
+
+}
