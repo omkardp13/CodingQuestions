075b48231a73a28eb0e1402a613c0fcb3d35a35f
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-24 19:42:51 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/UsingRecursive/FibonacciSeries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        
+    class program
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static void Main(string[] args) 
+        {
+            Console.Write("Enter the number of terms: ");
+            int terms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci Series:");
+            for (int i = 0; i < terms; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
