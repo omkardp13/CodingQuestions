708e3379060f7fb26bf4fff7d3cbc95e3765f20d
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-24 08:43:39 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/ReverseSentence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        
+    class program
+    {
+      public static void Main(string[] args) 
+        {
+            string sourceString = "my name is onkar dattatray patil";
+            string[] arr = sourceString.Split(' ');
+            for(int i=0;i<arr.Length/2;i++)
+            {
+                string temp = arr[i];
+                arr[i] = arr[arr.Length-i-1];
+                arr[arr.Length - i - 1]=temp;
+
+            }
+            string result = String.Join(" ", arr);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+
+}
