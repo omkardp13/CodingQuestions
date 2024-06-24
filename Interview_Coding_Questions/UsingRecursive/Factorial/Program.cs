3391889601cae8c6240b9df9c65d73c6df048a10
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-24 09:20:00 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/UsingRecursive/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        
+    class program
+    {
+        public static int CalFact(int num)
+        {
+            if (num == 0)
+                return 1;
+
+            return num*CalFact(num-1);
+        }
+      public static void Main(string[] args) 
+        {
+            int num = 5;
+           Console.WriteLine(CalFact(num));
+            Console.ReadLine();
+
+        }
+    }
+
+}
