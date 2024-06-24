0c806d1fde9446b0a90d610a47718d3837a9a88b
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-24 20:47:06 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/UsingRecursive/CountVowels/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+        
+    class program
+    {
+
+        public static int CountVowels(string s)
+        {
+            if (s.Length == 0)
+                return 0;
+
+            char firstChar = s[0];
+            int count = (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u' ||
+                         firstChar == 'A' || firstChar == 'E' || firstChar == 'I' || firstChar == 'O' || firstChar == 'U') ? 1 : 0;
+
+            return count + CountVowels(s.Substring(1));
+        }
+
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter String:");
+            string str=Console.ReadLine();
+            Console.WriteLine(CountVowels(str));
+           
+            Console.ReadLine();
+        }
+    }
+
+}
