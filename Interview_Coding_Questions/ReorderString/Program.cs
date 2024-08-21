5cf0f04f170d33736e1e3845f44b8a77d6c0b8c7
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-08-21 12:24:16 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/ReorderString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string order = "bca";
+        string str = "abc";
+
+        
+        Dictionary<char, char> charDict = new Dictionary<char, char>();
+
+        foreach (char c in str)
+        {
+            if (!charDict.ContainsKey(c))
+            {
+                charDict[c] = c;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+       
+        foreach (char c in order)
+        {
+            if (charDict.ContainsKey(c))
+            {
+                sb.Append(charDict[c]);
+            }
+        }
+
+        Console.WriteLine(sb.ToString());
+        Console.ReadLine();
+    }
+}
