86d3abb22aac3dc3805e636f60ffb25f36a4d37d
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-08-24 20:32:41 +0530

    Add files via upload

---
+++ DSA/LeetCode/0001-0099/0035/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+}
