de2a6910ca5aecd904ee88d2dc5e6b6a4d1cf092
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-08-24 12:53:06 +0530

    Add files via upload

---
+++ DSA/LeetCode/0001-0099/0027/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+
+    public int RemoveElement(int[] nums, int val)
+    {
+
+        int counter = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
