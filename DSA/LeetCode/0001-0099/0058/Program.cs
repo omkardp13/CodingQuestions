7ef4e02d152cf778ecb2e385746282ca3612be05
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-08-24 20:45:44 +0530

    Add files via upload

---
+++ DSA/LeetCode/0001-0099/0058/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution
+{
+
+    public static int LengthOfLastWord(string s)
+    {
+        string[] words = s.Split(' ');
+        int counter = words.Length - 1;
+        while (counter>=0)
+        {
+            if (words[counter]!=string.Empty)
+            {
+                return words[counter].Length;
+            }
+            counter--;
+        }
+
+        return -1;
+    }
+}
