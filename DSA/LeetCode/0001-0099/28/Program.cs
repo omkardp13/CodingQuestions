ace073e7a274ee9b87aa0f539cc8cce466b885b0
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-08-24 16:43:53 +0530

    Add files via upload

---
+++ DSA/LeetCode/0001-0099/28/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (haystack.Length < needle.Length)
+            return -1;
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] == needle[0])
+            {
+                if (CompareStrings(i, haystack, needle))
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public bool CompareStrings(int index, string haystack, string needle)
+    {
+        int counter = 0;
+
+
+
+        if ((index + needle.Length) > haystack.Length)
+            return false;
+
+        for (int i = index; i < index + (needle.Length); i++)
+        {
+            if (haystack[i] != needle[counter])
+            {
+                return false;
+            }
+            counter++;
+        }
+        return true;
+    }
+}
