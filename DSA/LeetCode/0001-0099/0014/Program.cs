1884eee1fcd7cc359a20e864a406a2e66d24fd24
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-08-26 19:14:54 +0530

    Add files via upload

---
+++ DSA/LeetCode/0001-0099/0014/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Headers;
+
+public class Solution
+{
+
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        Array.Sort(strs);
+        int index = 0;
+        string str1 = strs[0];
+        string str2 = strs[strs.Length-1];
+
+        for (int i=0;i<str1.Length;i++)
+        {
+            if (str1[i] == str2[i])
+                index++;
+            else
+                break;
+        }
+
+        return index == 0 ? "" :str1.Substring(0, index) ;
+    }
+}
