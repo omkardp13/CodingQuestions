f4a51eac227c926e43e8dfa136e14900517d0625
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-08-27 20:24:21 +0530

    Add files via upload

---
+++ DSA/LeetCode/0001-0099/0007/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+public class Solution
+{
+
+   
+    public static int Reverse(int x)
+    {
+
+       
+
+        int temp = x;
+        int res = 0;
+        while(temp!=0)
+        {
+            int rem = temp % 10;
+            res = res * 10+rem;
+
+            if(res<int.MinValue/10 && res>int.MaxValue/10)
+            {
+                return 0;
+            }
+            temp = temp / 10;
+        }
+
+        return res;
+    }
+
+}
