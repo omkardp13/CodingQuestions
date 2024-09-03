833a9305af590d5ec54279f57a126c6ca4df6026
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-09-03 12:11:59 +0530

    Add files via upload

---
+++ DSA/LeetCode/0200-0299/0283/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int count = 0;
+        if (nums.Length == 1)
+            return;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[count++] = nums[i];
+            }
+        }
+
+        while (count < nums.Length)
+        {
+            nums[count++] = 0;
+
+        }
+    }
+}
