27059511a02a7ed446941b94ff3fa615e4bf7f4d
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-09-04 12:52:21 +0530

    Add files via upload

---
+++ DSA/LeetCode/0200-0299/0217/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Solution
+{
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> visited = new HashSet<int>();
+
+        for(int i=0;i<nums.Length;i++)
+        {
+            if (visited.Contains(nums[i]))
+                return true;
+            else
+                visited.Add(nums[i]);
+        }
+
+        return false;
+    }
+
+
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 1 };
+        ContainsDuplicate(nums);
+
+
+    }
+}
