3f528f312415786ebe02a5e657719452127892b7
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-09-05 12:12:51 +0530

    Add files via upload

---
+++ DSA/LeetCode/0200-0299/0268/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+public class Solution
+{
+
+    public static int MissingNumber(int[] nums)
+    {
+        int sum = 0;
+
+        for(int i=0;i<nums.Length;i++)
+        {
+            sum += nums[i];
+        }
+
+        int ActualSum = nums.Length * (nums.Length + 1) / 2;
+
+        return ActualSum - sum;
+    }
+
+
+}
