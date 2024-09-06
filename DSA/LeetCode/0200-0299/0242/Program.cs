df087aed9423c014b9d726fbc7cdd5ee0f9a8656
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-09-06 09:09:02 +0530

    Add files via upload

---
+++ DSA/LeetCode/0200-0299/0242/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        if (s.Length > t.Length)
+            return false;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.ContainsKey(s[i]))
+            {
+                dict[s[i]]++;
+            }
+            else
+            {
+                dict.Add(s[i], 1);
+            }
+        }
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (dict.ContainsKey(t[i]))
+            {
+                if (dict[t[i]] != 0)
+                    dict[t[i]]--;
+                else
+                    return false;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        foreach (var item in dict)
+        {
+            if (item.Value > 1)
+            { return false; }
+        }
+
+        return true;
+    }
+}
