ae6963d57c14a81f9fe66db8ae8360044c3582d9
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-17 16:41:57 +0530

    Update Program.cs

--- Interview_Coding_Questions/Fibonacci_Series/Program.cs
+++ Interview_Coding_Questions/Fibonacci_Series/Program.cs
@@ -45,31 +45,6 @@ public class Program
         Console.Read();
     }
 }
-*/
 
 
-/*using recursion
- * 
- * 
-using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int n = 10; // Number of Fibonacci numbers to generate
-        Console.WriteLine("Fibonacci Series:");
-        PrintFibonacciSeries(n, 0, 1);
-        Console.ReadKey();
-    }
-
-    static void PrintFibonacciSeries(int n, int a, int b)
-    {
-        if (n > 0)
-        {
-            Console.Write(a + " ");
-            PrintFibonacciSeries(n - 1, b, a + b);
-        }
-    }
-}
-*/
