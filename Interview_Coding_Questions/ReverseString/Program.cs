26e1834c80242b737ff09335a8c6ce518e05e740
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-17 20:31:09 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Hello World";
+
+        // Convert the string into a character array
+        char[] arr = str.ToCharArray();
+
+        // Reverse the array manually using a loop
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+            // Swap characters
+            char c = arr[arr.Length - i - 1];
+            arr[arr.Length - i - 1] = arr[i];
+            arr[i] = c;
+        }
+
+        // Convert the character array back to a string and print it
+        Console.WriteLine(new string(arr));  // Output: "dlroW olleH"
+        Console.ReadLine();
+    }
+}
