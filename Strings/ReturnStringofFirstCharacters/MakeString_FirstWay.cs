ace73c033d8074320a16300c820c36877c3dce37
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 00:02:35 +0530

    Add files via upload

---
+++ Strings/ReturnStringofFirstCharacters/MakeString_FirstWay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+/*
+
+overall time complexity is O(n).
+
+overall space complexity is O(n).
+
+*/
+
+
+
+class Program
+{
+    static void Main()
+    {
+        string input = "This Is A Test";
+        string result = GetFirstCharacters(input);
+        Console.WriteLine(result); // Output: TIAT
+    }
+
+    static string GetFirstCharacters(string input)
+    {
+        // Split the string into words using space as a delimiter, then select the first character of each word.
+        return string.Concat(input.Split(' ').Select(word => word[0]));
+    }
+}
