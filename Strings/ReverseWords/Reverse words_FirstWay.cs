e120902382ef6595aa900d8ee13c823439512277
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 11:35:26 +0530

    Add files via upload

---
+++ Strings/ReverseWords/Reverse words_FirstWay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Example
+{
+    public static string ReverseWords(string str)
+    {
+        return string.Join(" ", str.Split(' ').Select(x =>new string(x.Reverse().ToArray())));
+    }
+
+    public static void Main()
+    {
+        string str = "This is an example!";
+        Console.WriteLine(ReverseWords(str));
+        Console.ReadLine();
+    }
+}
