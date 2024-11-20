5dc4a943be5255ea5f97a3051b5425406b940469
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 10:33:02 +0530

    Add files via upload

---
+++ Strings/CountingDuplicates/DuplicateCount_SecondWay.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+
+    /*
+     * 
+     * Time Complexity: O(n)
+
+       Space Complexity: O(n)
+     * 
+ 
+     */
+    public static int DuplicateCount(string str)
+    {
+        
+        int count = 0;
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        str=str.ToLower();
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (!dict.ContainsKey(str[i]))
+            {
+                dict.Add(str[i], 1);
+            }
+            else
+            {
+                dict[str[i]]++;
+            }
+        }
+
+        foreach (var keyValue in dict)
+        {
+            if (keyValue.Value > 1)
+                count++;
+        }
+        return count;
+    }
+    static void Main()
+    {
+
+        string str = "aabBcde";
+
+        Console.WriteLine(DuplicateCount(str));
+        Console.ReadLine();
+    }
+
+   
+}
