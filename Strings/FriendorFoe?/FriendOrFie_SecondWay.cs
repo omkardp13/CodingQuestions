24681f6bbbbfac04f5713f1b2ecd2ee84f38044e
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 11:15:45 +0530

    Add files via upload

---
+++ Strings/FriendorFoe?/FriendOrFie_SecondWay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+
+   public static IEnumerable<string> FriendOrFie(string[] names)
+    {
+        List<string> result = new List<string>();
+       foreach (string name in names)
+        {
+            if(name.Length==4)
+            {
+                result.Add(name);
+            }
+        }
+       return result;
+    }
+    
+    static void Main()
+    {
+
+
+       string[] Input = { "Ryan", "Kieran", "Jason", "Yous"};
+        foreach (string input in FriendOrFie(Input))
+        {
+            Console.WriteLine(input);
+        }
+        Console.ReadLine();
+    }
+
+   
+}
