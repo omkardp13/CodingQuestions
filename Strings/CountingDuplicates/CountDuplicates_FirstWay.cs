7d5e9d2fd154660fd7d643caa0cb8881d793aa62
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 10:40:01 +0530

    Add files via upload

---
+++ Strings/CountingDuplicates/CountDuplicates_FirstWay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+
+    /*
+     * 
+     * Time Complexity: O(n)
+
+       Space Complexity: O(n)
+     * 
+ 
+     */
+    public static int CountDuplicates(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return 0;
+
+        return input
+            .ToLower() // Convert the input to lowercase for case-insensitivity
+            .GroupBy(c => c) // Group by each character
+            .Count(group => group.Count() > 1); // Count groups where the frequency is greater than 1
+    }
+    static void Main()
+    {
+
+        string str = "aabBcde";
+
+        Console.WriteLine(CountDuplicates(str));
+        Console.ReadLine();
+    }
+
+   
+}
