4dc212fe3c0642a654174eb1771e4b279fe84eb5
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 11:21:04 +0530

    Add files via upload

---
+++ Strings/FriendorFoe?/FriendOrFie_FirstWay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FriendsFilter
+{
+    public static List<string> GetFriends(List<string> names)
+    {
+        // Filter the names that have exactly 4 letters
+        return names.Where(name => name.Length == 4).ToList();
+    }
+
+    public static void Main()
+    {
+        var input1 = new List<string> { "Ryan", "Kieran", "Jason", "Yous" };
+        var output1 = GetFriends(input1);
+        Console.WriteLine($"Input: {string.Join(", ", input1)}");
+        Console.WriteLine($"Output: {string.Join(", ", output1)}");
+
+        var input2 = new List<string> { "Peter", "Stephen", "Joe" };
+        var output2 = GetFriends(input2);
+        Console.WriteLine($"Input: {string.Join(", ", input2)}");
+        Console.WriteLine($"Output: {string.Join(", ", output2)}");
+        Console.Read();
+    }
+}
