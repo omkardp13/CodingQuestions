6b071cc9326f71bf52e68831db7b914044992fe7
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 15:06:12 +0530

    Add files via upload

---
+++ Strings/YourOrderPlease/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Example
+{
+
+    public static string Order(string words)
+    {
+       
+        var result=words.Split(' ').OrderBy(word=>word.FirstOrDefault(Char.IsDigit)).ToArray();
+
+        return string.Join(" ", result);
+    }
+
+
+    public static void Main()
+    {
+        string str = "is2 Thi1s T4est 3a";
+        Console.WriteLine(Order(str));
+
+
+
+        Console.ReadLine();
+    }
+}
