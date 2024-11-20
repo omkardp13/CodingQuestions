5ea766e35ff349f1006188687c9384b7d60783aa
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-20 14:51:01 +0530

    Add files via upload

---
+++ Strings/Exes and Ohs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Example
+{
+
+    public static bool XO(string input)
+    {
+        input = input.ToLower();
+        int xCount=input.Count(x => x=='x');
+
+        int oCount = input.Count(x => x == 'o');
+
+        if(xCount==oCount)
+        {
+            return true;
+        }
+
+        return false;
+  }
+
+
+    public static void Main()
+    {
+        string str = "ooxx";
+
+        if(XO(str))
+        {
+            Console.WriteLine("Balanced");
+        }
+        else
+        {
+            Console.WriteLine("Not Balanced");
+        }
+        
+        Console.ReadLine();
+    }
+}
