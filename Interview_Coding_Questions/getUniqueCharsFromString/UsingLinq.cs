2343cecd20ea84538e8a44d0e229a98cae8e7f5f
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-22 07:43:56 +0530

    Add files via upload

---
+++ Interview_Coding_Questions/getUniqueCharsFromString/UsingLinq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbstractClass
+{
+
+   
+    class Program
+    {
+
+     
+        public static void Main(string[] args)
+        {
+            string str = "Hello World";
+
+            var uniqueCharactors = str.GroupBy(c => c).Where(group => group.Count() == 1).Select(g => g.Key).ToList();
+
+
+            Console.WriteLine(string.Join(",",uniqueCharactors));
+            Console.ReadLine();
+        }
+    }
+}
