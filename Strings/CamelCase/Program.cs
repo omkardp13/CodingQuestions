b65f37886a9e81bc7a436464c8ba0543153fc749
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-25 09:36:16 +0530

    Add files via upload

---
+++ Strings/CamelCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbstractClass
+{
+      class Program
+      {
+
+        public static int camelcase(string s)
+        {
+            int countWordInCamelCase = 0;
+
+            for(int i=0;i<s.Length;i++)
+            {
+                if (i==0 && Char.IsLower(s[0]))
+                    countWordInCamelCase++;
+                if (Char.IsUpper(s[i]))
+                    countWordInCamelCase++;
+            }
+
+            return countWordInCamelCase;
+        }
+        public static void Main(string[] args)
+         {
+            string str = "saveChangesInTheEditor";
+            Console.WriteLine(camelcase(str));
+            Console.Read();  
+         }
+      }
+}
