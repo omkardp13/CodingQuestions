06d1c2eb3460df4ce9e7dab2538c2a6ea96116cd
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-25 07:47:43 +0530

    Add files via upload

---
+++ Strings/SuperReducedString/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbstractClass
+{
+      class Program
+      {
+
+        static string superReducedString(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(stack.Count==0)
+                {
+                    stack.Push(s[i]);
+                }
+                else
+                {
+                    char temp=stack.Peek();
+                    if (temp == s[i])
+                        stack.Pop();
+                    else
+                        stack.Push(s[i]);
+                }
+            }
+
+            return new string(stack.Reverse().ToArray());
+
+        }
+        public static void Main(string[] args)
+         {
+            //string str = "aab";
+
+           // string str = "aaabccddd";
+           // string str = "aa";
+           // string str = "baab";
+            string str = "aab";
+
+            Console.WriteLine(superReducedString(str));
+            Console.Read();  
+         }
+      }
+}
